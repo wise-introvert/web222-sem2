00062e5234e786221a754c706d52d63e10b80f8d
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2022-03-06 23:16:33 -0500

    feat: assignment 3 solution

---
+++ assignments/a3/assignment3/assignment3/.prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  /* Use 100 columns to format line width */
+  printWidth: 100,
+  /* Use 2 spaces to indent */
+  tabWidth: 2,
+  /* Always use semicolons */
+  semi: true,
+  /* Use spaces instead of tabs */
+  useTabs: false,
+  /* Use single quotes instead of double */
+  singleQuote: true,
+  /* Don't use trailing commas */
+  trailingComma: 'none',
+  /* Normalize and use whatever end of line character the user is using */
+  endOfLine: 'auto'
+};
